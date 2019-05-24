72667e0ad59576f780a2ef0d764b52c81b1bcbb0
baps <baps@bapss-MacBook-Pro-2.local>
2019-05-24 12:10:09 +0530

    added mixins

---
+++ app/styles/settings/mixins.js
@@ -0,0 +1,15 @@
+const textInput = props => `
+  color: ${props.error ? color.white : color.base};
+  background-color: ${props.error ? color.alert : color.white};
+`;
+
+export const Input = styled.input`
+  ${textInput}
+`;
+
+export const Textarea = styled.textarea`
+  ${textInput};
+  height: ${props => props.height ? props.height : '130px'}
+  resize: none;
+  overflow: auto;
+`;
